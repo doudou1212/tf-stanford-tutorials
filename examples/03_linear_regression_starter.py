b1fa866f5d8dfb6083dfe3e144f9acd99595bf59
jing sun <jing.sun@rea-group.com>
2017-04-17 08:52:46 +0800

    2nd homework

--- examples/03_linear_regression_starter.py
+++ examples/03_linear_regression_starter.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import xlrd
 
-DATA_FILE = 'data/fire_theft.xls'
+DATA_FILE = '/Users/sunjing/MLHomeWorks/tf-stanford-tutorials/data/fire_theft.xls'
 
 # Phase 1: Assemble the graph
 # Step 1: read in data from the .xls file
@@ -19,39 +19,53 @@ data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
 n_samples = sheet.nrows - 1
 
 # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
+X = tf.placeholder(dtype=tf.float32, name='fire')
+Y = tf.placeholder(dtype=tf.float32, name='theft')
 
 
 # Step 3: create weight and bias, initialized to 0
 # name your variables w and b
+w = tf.Variable(0.0, name="weight")
+b = tf.Variable(0.0, name="bias")
 
 
 # Step 4: predict Y (number of theft) from the number of fire
 # name your variable Y_predicted
+y_predicted = tf.add(tf.multiply(X,w), b, name="Y_predicted")
 
 
 # Step 5: use the square error as the loss function
 # name your variable loss
+loss = tf.square(tf.subtract(Y, y_predicted), name="loss")
 
 # Step 6: using gradient descent with learning rate of 0.01 to minimize loss
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0002).minimize(loss)
  
 # Phase 2: Train our model
 with tf.Session() as sess:
 	# Step 7: initialize the necessary variables, in this case, w and b
-	# TO - DO	
+	# TO - DO
+	sess.run(tf.global_variables_initializer())
+
+	writer = tf.summary.FileWriter('./my_graph/03/linear_reg', sess.graph)
 
 	# Step 8: train the model
-	for i in range(100): # run 100 epochs
+	for i in range(1000): # run 100 epochs
 		total_loss = 0
 		for x, y in data:
 			# Session runs optimizer to minimize loss and fetch the value of loss
 			# TO DO: write sess.run()
+			_, l = sess.run([optimizer, loss], feed_dict={X: x, Y:y})
 			total_loss += l
 		print "Epoch {0}: {1}".format(i, total_loss/n_samples)
+
+	writer.close()
+
+	W, B = sess.run([w, b])
 	
-# plot the results
-# X, Y = data.T[0], data.T[1]
-# plt.plot(X, Y, 'bo', label='Real data')
-# plt.plot(X, X * w + b, 'r', label='Predicted data')
-# plt.legend()
-# plt.show()
+#plot the results
+X, Y = data.T[0], data.T[1]
+plt.plot(X, Y, 'bo', label='Real data')
+plt.plot(X, X * W + B, 'r', label='Predicted data')
+plt.legend()
+plt.show()
--- examples/03_logistic_regression_mnist_starter.py
+++ examples/03_logistic_regression_mnist_starter.py
@@ -11,24 +11,28 @@ from tensorflow.examples.tutorials.mnist import input_data
 import time
 
 # Define paramaters for the model
-learning_rate = 0.01
+learning_rate = .01
 batch_size = 128
-n_epochs = 10
+n_epochs = 40
 
 # Step 1: Read in data
 # using TF Learn's built in function to load MNIST data to the folder data/mnist
-mnist = input_data.read_data_sets('/data/mnist', one_hot=True) 
+mnist = input_data.read_data_sets('/Users/sunjing/MLHomeWorks/tf-stanford-tutorials/data', one_hot=True)
 
 # Step 2: create placeholders for features and labels
 # each image in the MNIST data is of shape 28*28 = 784
 # therefore, each image is represented with a 1x784 tensor
-# there are 10 classes for each image, corresponding to digits 0 - 9. 
+# there are 10 classes for each image, corresponding to digits 0 - 9.
+X = tf.placeholder(tf.float32, [batch_size, 784], name="x_placeholder")
+Y = tf.placeholder(tf.float32, [batch_size, 10], name="y_placeholder")
 
 
 # Step 3: create weights and bias
 # weights and biases are initialized to 0
 # shape of w depends on the dimension of X and Y so that Y = X * w + b
 # shape of b depends on Y
+w = tf.Variable(tf.random_normal(shape=[784, 10], stddev=0.01), name='weights')
+b = tf.Variable(tf.zeros([1, 10]), name='bias')
 
 
 # Step 4: build model
@@ -36,17 +40,21 @@ mnist = input_data.read_data_sets('/data/mnist', one_hot=True)
 # this logits will be later passed through softmax layer
 # to get the probability distribution of possible label of the image
 # DO NOT DO SOFTMAX HERE
-
+logits = tf.matmul(X, w) + b
 
 # Step 5: define loss function
 # use cross entropy loss of the real labels with the softmax of logits
 # use the method:
 # tf.nn.softmax_cross_entropy_with_logits(logits, Y)
 # then use tf.reduce_mean to get the mean loss of the batch
+entropy = tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=logits, name="loss")
+loss = tf.reduce_mean(entropy)
+
 
 
 # Step 6: define training op
 # using gradient descent to minimize loss
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
 
 with tf.Session() as sess:
@@ -59,8 +67,7 @@ with tf.Session() as sess:
 		for _ in range(n_batches):
 			X_batch, Y_batch = mnist.train.next_batch(batch_size)
 			# TO-DO: run optimizer + fetch loss_batch
-			# 
-			# 
+			_, loss_batch = sess.run([optimizer, loss], feed_dict={X: X_batch, Y:Y_batch})
 			total_loss += loss_batch
 		print 'Average loss epoch {0}: {1}'.format(i, total_loss/n_batches)
 
